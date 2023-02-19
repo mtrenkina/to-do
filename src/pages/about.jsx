77eaa98e32366e4c724b52ef8b3250909011b3c5
Margarita Trenkina <margaritatrenkina@gmail.com>
2023-02-19 22:03:44 +0300

    Start

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About page</h1>
+        </React.Fragment>
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Home = () => {
+    return (
+        <React.Fragment>
+            <h1>Home page</h1>
+        </React.Fragment>
+    )
+}
